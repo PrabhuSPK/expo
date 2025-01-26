9b56a9e0b13c3432851d9e2c0b4b346c7a1c52f9
Prabhu Selvamani <prabhuselvamani1995@gmail.com>
2025-01-26 21:16:43 +0530

    Expo Router Setup

---
+++ ecom-app/app/contact.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function contact() {
+  return (
+    <View>
+      <Text>contact page</Text>
+      <Text>contact page</Text>
+      <Text>contact page</Text>
+    </View>
+  )
+}
